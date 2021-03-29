75df15af8ce83d98e014d0e9fad61ca81c5e1e22
Tristan Nelson <Tristan@tristans-mbp.lan>
2021-03-29 16:02:58 -0400

    working on atlas cluster and mongoose connection

--- server.js
+++ server.js
@@ -4,11 +4,27 @@ const port = process.env.PORT || 5000;
 
 const staticDir = path.resolve("./client/public");
 
+//global import of mongoose schema
+const MagicModel = require("./mongoose.js");
+
 //-------SERVER SETUP----------//
 const app = express();
 
 //middleware for helping read body of post request
 app.use(express.urlencoded({ extended: true }));
+
+app.get("*", async (req, res) => {
+  const cursor = await MagicModel.find({});
+  console.log("Curser =", cursor);
+
+  let results = [];
+  //iterate through each document in collection and push it into array
+  await cursor.forEach((entry) => {
+    results.push(entry);
+  });
+
+  res.json(results);
+});
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(staticDir + "/index.html"));
