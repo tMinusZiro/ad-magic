53ec3a39efa02530939016374455dfeded971e26
meganzinka <meganzinka@gmail.com>
2021-04-04 14:14:51 -0400

    merge issues figured out, data displays are MIA

--- client/src/components/RightDash.jsx
+++ client/src/components/RightDash.jsx
@@ -4,32 +4,29 @@ import {useState, useEffect} from 'react'
 
 
 
-// export default function RightDash() {
-//  const[SaleTypes, setSaleTypes] = useState([])
+export default function RightDash() {
+ const[SaleTypes, setSaleTypes] = useState([])
     
-// async function vendorsList(){
-//     console.log('in use effect RIGTH DASH')
-//     console.log('in trigger RIGHT DASH')
-// useEffect(() => {
-//     console.log('in use effect')
-    
-//         console.log('in trigger')
-//     fetch('/vendors')
-//     .then((res) =>res.json())
-//     .then((entry)=>{
-//         setSaleTypes(entry)
-//     })
-// }
-// , []
-// )
-// // }
+async function vendorsList(){
+    console.log('in use effect RIGTH DASH')
+    console.log('in trigger RIGHT DASH')
+useEffect(() => {
+    fetch('/vendors')
+    .then((res) =>res.json())
+    .then((entry)=>{
+        setSaleTypes(entry)
+    })
+}
+, []
+)
+}
 
-// // vendorsList()
+// vendorsList()
 
 
-//     return (
-//         <div>
-//        <Vendors SaleTypes={SaleTypes} />
-//         </div>
-//     )
-// }
+    return (
+        <div>
+       <Vendors SaleTypes={SaleTypes} />
+        </div>
+    )
+}
