312a775cc222d4f1f12b8b0e0f7b5ce8548145ea
Tristan Nelson <Tristan@tristans-mbp.lan>
2021-04-05 12:30:12 -0400

    fixing merge conflicts

--- client/src/components/MapLegend.jsx
+++ client/src/components/MapLegend.jsx
@@ -1,48 +1,42 @@
 import React from "react";
-import {useState, useEffect} from "react"
-import ClientPopUp from "./ClientPopUp"
-const MapLegend = ( {legendItems}) => {
-
-  const [min, setMin] = useState()
-  const [max, setMax] = useState()
-  const [clientListInRange, setClientListInRange] = useState()
-  const [showClients, setShowClients] = useState(false)
+import { useState, useEffect } from "react";
+import ClientPopUp from "./ClientPopUp";
+const MapLegend = ({ legendItems, openLegend, setOpenLegend }) => {
+  console.log(legendItems);
+  const [min, setMin] = useState();
+  const [max, setMax] = useState();
+  const [clientListInRange, setClientListInRange] = useState();
+  const [showClients, setShowClients] = useState(false);
 
   useEffect(() => {
     fetch(`/client/${min}/${max}`)
-    .then((res) => res.json())
-    .then((list) => {
-      setClientListInRange(list)
-    })
-  }, [min])
+      .then((res) => res.json())
+      .then((list) => {
+        setClientListInRange(list);
+      });
+  }, [min]);
 
-  console.log(clientListInRange)
+  console.log(clientListInRange);
 
-  function getClients (event) {
-    let itemRange = event.target.value
-    console.log(event.target.value.isFor)
+  function getClients(event) {
+    let itemRange = event.target.value;
+    console.log(event.target.value.isFor);
     if (itemRange === "0") {
-      setMin(0)
-      setMax(0)
+      setMin(0);
+      setMax(0);
     } else if (itemRange.includes("+")) {
-      itemRange = itemRange.split(" ")
-      itemRange = itemRange[0]
-      itemRange = itemRange.replace(",", "")
-      setMin(parseInt(itemRange))
-      setMax(100000000000)
+      itemRange = itemRange.split(" ");
+      itemRange = itemRange[0];
+      itemRange = itemRange.replace(",", "");
+      setMin(parseInt(itemRange));
+      setMax(100000000000);
     } else {
-      itemRange = itemRange.split(" - ")
-      setMin(parseInt(itemRange[0].replace(",", "")))
-      setMax(parseInt(itemRange[1].replace(",", "")))
+      itemRange = itemRange.split(" - ");
+      setMin(parseInt(itemRange[0].replace(",", "")));
+      setMax(parseInt(itemRange[1].replace(",", "")));
     }
-    setShowClients(true)
+    setShowClients(true);
   }
-
-<<<<<<< HEAD
-const MapLegend = ({ legendItems, openLegend, setOpenLegend }) => {
-  console.log(legendItems);
-=======
->>>>>>> new-sidebar
 
   function flipMenu() {
     openLegend ? setOpenLegend(!openLegend) : setOpenLegend(openLegend);
@@ -68,10 +62,10 @@ const MapLegend = ({ legendItems, openLegend, setOpenLegend }) => {
     >
       {legendItems.map((item, index) => (
         <button
-          onClick= {getClients}
+          onClick={getClients}
           key={index}
-          value ={item.title}
-          name = {item.title}
+          value={item.title}
+          name={item.title}
           style={{
             backgroundColor: item.color,
             flex: 1,
@@ -87,9 +81,15 @@ const MapLegend = ({ legendItems, openLegend, setOpenLegend }) => {
           {item.title}
         </button>
       ))}
-      {showClients ? 
-      (<ClientPopUp min = {min} max = {max} clientListInRange = {clientListInRange} setShowClients= {setShowClients} showClients = {showClients}/>)
-      : null }
+      {showClients ? (
+        <ClientPopUp
+          min={min}
+          max={max}
+          clientListInRange={clientListInRange}
+          setShowClients={setShowClients}
+          showClients={showClients}
+        />
+      ) : null}
     </div>
   );
 };
